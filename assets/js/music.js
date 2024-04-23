7e0693567834099a039ae27ecdaaf3814b1ba080
Dominic-wells <joedomwells@gmail.com>
2024-04-23 20:30:59 +0100

    added  comments for music

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,6 +1,7 @@
+// Adds music
 const music = document.getElementById("background-music");
 const musicToggle = document.getElementById("music-toggle");
-
+// this triggers the music to play or pause
 musicToggle.addEventListener("click", () => {
   if (music.paused) {
     music.play();
