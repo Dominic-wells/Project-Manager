77fc62e1112e1ce85d8d907e4ac0a4ce3a827ad7
Dominic-wells <joedomwells@gmail.com>
2024-04-29 10:29:20 +0100

    updated path fopr config.php in config folder

--- adminUsers.php
+++ adminUsers.php
@@ -47,7 +47,7 @@
     </header>
 
 <?php
-    include 'config.php';
+    include 'config/config.php';
     session_start();
     // The database connection file, this is to avoid storing the connection details in plain view
 
