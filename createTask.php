fcc1ae164bec5ee40fcf2f7495cc18d8d60cf043
Dominic-wells <joedomwells@gmail.com>
2024-04-29 10:19:27 +0100

    Updated filepath for config in config folder

--- createTask.php
+++ createTask.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Google Fonts -->
+    <!-- GoogleFonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@500&display=swap" rel="stylesheet">
@@ -39,12 +39,12 @@
             <button id="myBtn" onclick="myFunction()">Pause</button>
             <button id="music-toggle">Toggle Music</button>
         </div>
-        <h1>Manage Your Tasks</h1>
+        <h1>Create Your Task</h1>
     </header>
 
     <?php
-    include 'config.php';
     // The database connection file, this is to avoid storing the connection details in plain view
+    include 'config/config.php';
     session_start();
 
     // Redirect non-admin users who should not set assignees
